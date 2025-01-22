3c337761852b919d8dd5e01b29b60291250c9f51
Emerson Gonzalez <emerson.dvlmt@gmail.com>
2025-01-22 14:46:02 -0300

    Operators to Conditionals

---
+++ Basic/10-arrays.js
@@ -0,0 +1,11 @@
+// Arrays
+//
+let myArray = [];
+let myArray2 = new Array();
+console.log(myArray);
+console.log(myArray2);
+
+myArray = [3];
+myArray2 = new Array(3);
+console.log(myArray);
+console.log(myArray2);
